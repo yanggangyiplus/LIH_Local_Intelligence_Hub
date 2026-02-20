b6f008e221f4e544bcba8092e8195c4c6c7866df
HANGAYEONG <yanggangyiplus@gmail.com>
2026-02-20 10:35:18 +0900

    Delete LIH_TEST directory

--- LIH_TEST/app/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-LLM_PROVIDER = os.getenv("LLM_PROVIDER", "ollama")
-OLLAMA_BASE_URL = os.getenv("OLLAMA_BASE_URL", "http://localhost:11434")
-OLLAMA_CHAT_MODEL = os.getenv("OLLAMA_CHAT_MODEL", "llama3.2")
-OLLAMA_EMBEDDING_MODEL = os.getenv("OLLAMA_EMBEDDING_MODEL", "nomic-embed-text")
-CHROMA_PERSIST_DIR = os.getenv("CHROMA_PERSIST_DIR", "./data/chroma")
-SQLITE_DB_PATH = os.getenv("SQLITE_DB_PATH", "./data/lih.db")
